2be9937470aa20392e21603d90b06b92da298aa5
Vincent Guerandel <guerandelv@redcat.com.au>
2019-07-28 19:12:55 +1000

    Create project structure | Add react

---
+++ renderer.webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: './src/renderer/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    }
+  };
